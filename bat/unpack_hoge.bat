6ec9f48056a7d387427c37940a187edcd50aa5e4
luluci <lunacied@gmail.com>
2020-12-12 22:23:08 +0900

    fix: bat file.

--- bat/unpack_hoge.bat
+++ bat/unpack_hoge.bat
@@ -8,13 +8,13 @@ set RUN_CMD=""
 set SCRIPT_FILE=util7z\unpacker_hoge.py
 
 if exist ".venv" (
-	set RUN_CMD=pipenv run python %SCRIPT_FILE%
+	set RUN_CMD=pipenv run python %SCRIPT_FILE% %1%
 ) else (
-	set RUN_CMD=python %SCRIPT_FILE%
+	set RUN_CMD=python %SCRIPT_FILE% %1%
 )
 
 @echo %RUN_CMD%
 %RUN_CMD%
 
-
+@rem @pause
 @endlocal
